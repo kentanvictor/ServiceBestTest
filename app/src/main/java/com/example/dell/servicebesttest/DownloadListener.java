9d818a2baa2a41be0126e745c98f03b5e8aedd84
kentanvictor <2030457926@qq.com>
2017-06-09 22:36:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/servicebesttest/DownloadListener.java
@@ -0,0 +1,13 @@
+package com.example.dell.servicebesttest;
+
+/**
+ * Created by DELL on 2017/6/9.
+ */
+
+public interface DownloadListener {
+    void onProgress(int progress);//用于通知当前下载进度
+    void onSuccess();//用于通知下载成功事件
+    void onFailed();//用于通知下载失败事件
+    void onPaused();//用于通知下载暂停事件
+    void onCanceled();//用于通知下载取消事件
+}
