a342ebbf8b620189c5de94bcb0692148f7c06418
kentanvictor <2030457926@qq.com>
2017-06-11 22:05:43 +0800

    ninth commit

--- app/src/main/java/com/example/dell/servicebesttest/MainActivity.java
+++ app/src/main/java/com/example/dell/servicebesttest/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.dell.servicebesttest;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.Connection;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private DownloadService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            downloadBinder = (DownloadService.DownloadBinder)Service;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(downloadBinder == null)
+        {
+            return;
+        }switch (v.getId())
+        {
+            case R.id.start_download:
+                String url = "https://raw.githubsercontent.com/guolindev/eclipes/master/eclipes-inst-win64.exe";
+                downloadBinder.startDownload(url);
+                break;
+            case R.id.pause_download:
+                downloadBinder.pauseDownload();
+                break;
+            case R.id.cancel_download:
+                downloadBinder.cancelDownload();
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, "拒绝权限将无法使用程序",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(Connection);
     }
 }
